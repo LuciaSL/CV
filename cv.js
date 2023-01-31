e7a4213aa608ee5019c0bebb64deaeb8a53d118d
LuciaSL <123827952+LuciaSL@users.noreply.github.com>
2023-01-31 13:19:32 -0300

    file js listo

---
+++ cv.js
@@ -0,0 +1,48 @@
+$(function() {
+	$("#visible1").click(function() {
+	    $("#invisible1").toggleClass("show");
+	  });  
+	}); 
+   
+   $(function() {
+	$("#visible2").click(function() {
+	    $("#invisible2").toggleClass("show");
+	  });  
+	}); 
+
+   $(function() {
+	$("#visible3").click(function() {
+	    $("#invisible3").toggleClass("show");
+	  });  
+	});
+
+   $(function() {
+	$("#visible4").click(function() {
+	    $("#invisible4").toggleClass("show");
+	  });  
+	}); 
+
+   $(function() {
+	$("#visible5").click(function() {
+	    $("#invisible5").toggleClass("show");
+	  });  
+	}); 
+
+   $(function() {
+	$("#visible6").click(function() {
+	    $("#invisible6").toggleClass("show");
+	  });  
+	}); 
+
+   $(function() {
+	$("#visible7").click(function() {
+	    $("#invisible7").toggleClass("show");
+	  });  
+	}); 
+
+   $(function() {
+	$("#visible8").click(function() {
+	    $("#invisible8").toggleClass("show");
+	  });  
+	}); 
+   
